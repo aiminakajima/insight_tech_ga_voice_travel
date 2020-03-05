0e8a9771ca3d874f7b3ce73b11fca82270d53bda
toriih <toriih@isao.co.jp>
2020-03-05 09:44:32 +0900

    feat: アーカイブ用スクリプト作成

--- _zip.sh
+++ _zip.sh
@@ -3,5 +3,5 @@
 date=`date "+%Y%m%d_%H%M%S"`
 #git_commit=`git rev-parse HEAD`
 git_commit=`git rev-parse --short HEAD`
-# zip "../_archives/google-conversation-action_${date}-${git_commit}.zip" -r ./ -x *.DS_Store *.git* *node_modules* ./functions/js\*
-zip "google-conversation-action_${date}.zip" -r ./ -x *.DS_Store *.git* *node_modules* ./functions/js\*
+# zip "../_archives/insightech_travel_voice_${date}-${git_commit}.zip" -r ./ -x *.DS_Store *.git* *node_modules* ./functions/js\*
+zip "./_archives/insightech_travel_voice_${date}.zip" -r ./ -x *.DS_Store *.git* *node_modules* ./functions/js\*
